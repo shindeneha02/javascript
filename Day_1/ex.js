f72a60b1806e93f9590425a734fc5bda96dee8b0
Neha Shantaram Shinde <32386135+shindeneha02@users.noreply.github.com>
2025-07-25 22:31:43 +0530

    javascript basics

--- Day_1/ex.js
+++ Day_1/ex.js
@@ -1,2 +1,16 @@
 var str1="hello world";
-console.log(str1.typeof(str1
+console.log(str1,typeof(str1));
+
+var num=1234;
+console.log(num, typeof(num));
+
+var bool=true;
+console.log(bool,typeof(bool));
+
+//------------------------------variables------------------------------------------------------------------
+
+var str1="hello";
+console.log(str1);
+
+let num1=132;
+console.log(num1)
